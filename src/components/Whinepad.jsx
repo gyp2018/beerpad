06787afced9b9666c2792d63cb5d072a70101ec4
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-22 23:28:27 +0900

    add Whinepad

---
+++ src/components/Whinepad.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Whinepad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.initialData,
+      addnew: false,
+    };
+    this.preSearchData = null;
+  }
+
+  addNewDialog() {
+
+  }
+
+  addNew(action) {
+
+  }
+
+  onExcelDataChange(data) {
+
+  }
+
+  commitToStorage() {
+
+  }
+
+  startSearching() {
+
+  }
+
+  doneSearching() {
+
+  }
+
+  search(e) {
+
+  }
+
+
+  render() {
+    return (
+
+    )
+  }
+}
