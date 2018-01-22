4efa2c2d47570fdade6ee207e5f8a0758bfced3c
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-22 22:36:57 +0900

    add Dialog

---
+++ src/components/Dialog.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+class Dialog extends React.Component {
+  componentWillUnmount() {
+    document.body.classList.remove('DialogModalOpen');
+  }
+
+  componentDidMount() {
+    if (this.props.modal) {
+      document.body.classList.add('DialogModalOpen');
+    }
+  }
+
+  render() {
+    return (
+      <div className={classNames({ 'Dialog': true, 'DialogModal': this.props.modal })}>
+        <div className={classNames({ 'DialogModalWrap': this.props.modal })}>
+          <div className="DialogHeader">{this.props.header}</div>
+          <div className="DialogBody">{this.props.children}</div>
+          <div className="DialogFooter">
+            {this.props.hasCancel
+              ? (
+                <span
+                  className="DialogDismiss"
+                  onClick={this.props.onAction.bind(this, 'dismiss')}
+                >
+                  Cancel
+                </span>
+              ) : null
+            }
+            <button
+              onClick={this.props.onAction.bind(this,
+                this.props.hasCancel ? 'confirm' : 'dismiss')
+              }
+            >
+              {this.props.confirmLabel}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dialog.propTypes = {
+  header: PropTypes.string.isRequired,
+  confirmLabel: PropTypes.string,
+  modal: PropTypes.bool,
+  onAction: PropTypes.func,
+  hasCancel: PropTypes.bool,
+};
+
+Dialog.defaultProps = {
+  confirmLabel: 'ok',
+  modal: false,
+  onAction: () => {},
+  hasCancel: true,
+};
+
+export default Dialog;
