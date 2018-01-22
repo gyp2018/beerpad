909eee320a664feb10737413ea6b5ccd04738985
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-22 20:55:07 +0900

    fixed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import BeerCard from './beerCard/BeerCard';
 import Button from './Button';
 import Suggest from './Suggest';
 import Rating from './Rating';
+import Form from './Form';
 
 const App = () => (
   <div className="container">
@@ -12,6 +13,15 @@ const App = () => (
     <Button href="https://bootstrap.com">Click</Button>
     <Suggest options={['hi', 'hello']} id="greeting" />
     <Rating id="greetingRating" />
+    <Form
+      fields={[
+        { label: 'Rating', type: 'rating', id: 'rateme' },
+        { label: 'Greeting', id: 'freetext' },
+      ]}
+      initialData={
+        { rateme: 4, freetext: 'Hello' }
+      }
+    />
   </div>
 );
 
