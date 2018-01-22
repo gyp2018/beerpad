6f57d58e9c3c9129ff625fbe29c0b3ed035dfe13
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-22 22:36:50 +0900

    add schema

---
+++ src/schema.js
@@ -0,0 +1,39 @@
+import classification from './classification';
+
+export default [
+  {
+    id: 'name',
+    label: 'Name',
+    show: true,
+    sample: '$2 chunk',
+    align: 'left',
+  },
+  {
+    id: 'year',
+    label: 'Year',
+    type: 'year',
+    show: true,
+    sample: 2015,
+  },
+  {
+    id: 'grape',
+    label: 'Grape',
+    type: 'suggest',
+    optoins: classification.grapes,
+    show: 'Merlot',
+    align: 'left',
+  },
+  {
+    id: 'rating',
+    label: 'Rating',
+    type: 'rating',
+    show: true,
+    sample: 3,
+  },
+  {
+    id: 'comments',
+    label: 'Comments',
+    type: 'text',
+    smaple: 'Nice for the price',
+  },
+]
