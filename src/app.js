17f377ea608772cf194fac5050e4fb08af367b75
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-23 23:33:23 +0900

    add app.js

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Whinepad from './components/Whinepad';
+import schema from './schema';
+
+let data = JSON.parse(localStorage.getItem('data'));
+
+if (!data) {
+  data = {};
+  schema.forEach(item => data[item.id] = item.sample);
+  data = [data];
+}
+
+ReactDOM.render(
+  <div>
+    <div className="app-header">
+      Logo
+    </div>
+    <Whinepad schema={schema} initialData={data} />
+  </div>,
+  document.getElementById('app')
+);
