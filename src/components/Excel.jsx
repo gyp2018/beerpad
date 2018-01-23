668698440de32d5bf3bc05241a488c183d4e0535
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-23 23:33:10 +0900

    fixed Excel

--- src/components/Excel.jsx
+++ src/components/Excel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Dialog from './Dialog';
 
 class Excel extends React.Component {
   constructor(props) {
@@ -12,6 +13,11 @@ class Excel extends React.Component {
       edit: null,
       dialog: null,
     };
+
+    this.deleteConfirmationClick = this.deleteConfirmationClick.bind(this);
+    this.saveDataDialog = this.saveDataDialog.bind(this);
+    this.sort = this.sort.bind(this);
+    this.showEditor =this.showEditor.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -71,35 +77,171 @@ class Excel extends React.Component {
   }
 
   deleteConfirmationClick(action) {
-
+    if (actoin === 'dismiss') {
+      this.closeDialog();
+      return;
+    }
+    let data = Array.from(this.state.data);
+    data.slice(this.state.dialog.idx, 1);
+    this.setState({
+      dialog: null,
+      data,
+    });
+    this.fireDataChange(data);
   }
 
   closeDialog() {
-
+    this.setState({
+      dialog: null,
+    });
   }
 
-  saveDataDialog() {
-
+  saveDataDialog(action) {
+    if (action === 'dismiss') {
+      this.closeDialog();
+      return;
+    }
+    let data = Array.from(this.state.data);
+    data[this.state.dialog.idx] = this.refs.form.getData();
+    this.setState({
+      dialog: null,
+      data,
+    });
+    this.fireDataChange(data);
   }
 
   render() {
-
+    return (
+      <div className="Excel">
+        {this.renderTable()}
+        {this.renderDialog()}
+      </div>
+    );
   }
-
+  
   renderDialog() {
-
+    if (!this.state.dialog) {
+      return null;
+    }
+    switch (this.state.dialog.type) {
+      case 'delete':
+        return this.renderDeleteDialog();
+      case 'info':
+        return this.renderFormDialog(true);
+      case 'eidt':
+        return this.renderFormDialog();
+      default:
+        throw Error(`Unexpected dialog type ${this.state.dialog.type}`);
+    }
   }
 
   renderDeleteDialog() {
-
+    const first = this.state.data[this.state.dialog.idx];
+    const nameguess = first[Object.keys(first)[0]];
+    return (
+      <Dialog
+        modal={true}
+        header="Confirm deletion"
+        confirmLabel="Delete"
+        onAction={this.deleteConfirmationClick}
+      >
+        {`Are you sure want to delete "${nameguess}"?`}
+      </Dialog>
+    );
   }
 
   renderFormDialog() {
-
+    return (
+      <Dialog
+        modal={true}
+        header={readonly ? 'Item info' : 'Edit item'}
+        confirmLabel={!readonly}
+        onAction={this.saveDataDialog}
+      >
+        <Form
+          ref="form"
+          fields={this.props.schema}
+          initialData={this.state.data[this.state.dialog.idx]}
+          readonly={readonly}
+        />
+      </Dialog>
+    );
   }
 
   renderTable() {
-
+    return (
+      <table>
+        <thead>
+          <tr>
+            {this.props.schema.map((item) => {
+              if (!item.show) {
+                return null;
+              }
+              let title = item.label;
+              if (this.state.sortby === item.id) {
+                title += this.state.descending ? ' \u2191' : ' \u2193';
+              }
+              return (
+                <th
+                  className={`schema-${item.id}`}
+                  key={item.id}
+                  onClick={() => {this.sort(item.id)}}
+                >
+                  {title}
+                </th>
+              );
+            })}
+            <th className="ExcelNotSortable">Actions</th>
+          </tr>
+        </thead>
+        <tbody onDoubleClick={this.showEditor}>
+          {this.state.data.map((row, rowidx) => {
+            return (
+              <tr key={rowidx}>
+                {Object.keys(row).map((cell, idx) => {
+                  const schema = this.props.schema[idx];
+                  if (!schema || !schema.show) {
+                    return null;
+                  }
+                  const isRating = schema.type === 'rating';
+                  const edit = schema.edit;
+                  let content = row[cell];
+                  if (!isRating && edit && edit.row === rowidx && edit.key === schema.id) {
+                    content = (
+                      <form onSubmit={this.save}>
+                        <FormInput ref="input" {...schema} defaultValue={content} />
+                      </form>
+                    );
+                  } else if (isRating) {
+                    content = <Rating readonly={true} defaultValue={Number(content)} />;
+                  }
+                  return (
+                    <td
+                      className={classNames({
+                        [`schema-${schema.id}`]: true,
+                        'ExcelEditable': !isRating,
+                        'ExcelDataLeft': schema.align === 'left',
+                        'ExcelDataRight': schema.align === 'right',
+                        'ExcelDataCenter': schema.align !== 'left' &&
+                          schema.align !== 'right',
+                      })}
+                      key={idx}
+                      data-row={rowidx}
+                      data-key={schema.id}
+                    >
+                      {content}
+                    </td>
+                  );
+                })}
+                <td>
+                  <Action onAction={() => this.actionClick(rowidx)} />
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    );
   }
 }
 
