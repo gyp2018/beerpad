b84101a339e93d58fd8a0b11c9f67ef41ff1370e
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-24 19:02:07 +0900

    fixed Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const Button = (props) => {
-  const cssclasses = classNames('btn', props.className);
+  const cssclasses = classNames('btn', props.className ? `btn-${props.className}` : 'btn-light');
   return props.href
     ? <a {...props} className={cssclasses}>{props.children}</a>
     : <button {...props} className={cssclasses}>{props.children}</button>;
