c4fa6bd453563a1b3f5fff3b5525b19a93bf25bc
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-24 19:13:04 +0900

    add font-awesome

--- src/vendor.js
+++ src/vendor.js
@@ -4,3 +4,4 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
