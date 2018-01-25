51774d7b24b30ed0922206b36ec6f39adaeb2ce5
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-25 12:20:06 +0900

    fixed Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import classnames from 'classnames';
 
 const Button = (props) => {
-  const cssclasses = classNames('btn', props.className ? `btn-${props.className}` : 'btn-light');
+  const outline = props.outline ? '-outline' : '';
+  const color = props.color || 'dark';
+  const size = props.size ? `btn-${props.size}` : null;
+  const cssclasses = classnames('btn', `btn${outline}-${color}`, size);
   return props.href
-    ? <a {...props} className={cssclasses}>{props.children}</a>
-    : <button {...props} className={cssclasses}>{props.children}</button>;
+    ? <a role="button" className={cssclasses} href={props.href}>{props.children}</a>
+    : <button type="button" className={cssclasses}>{props.children}</button>;
 };
 
 Button.propTypes = {
   href: PropTypes.string,
-  className: PropTypes.string,
+  color: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']),
+  outline: PropTypes.bool,
+  size: PropTypes.oneOf(['lg', 'sm']),
   children: PropTypes.string.isRequired,
-};
-Button.defaultProps = {
-  href: '',
-  className: '',
 };
 
 export default Button;
